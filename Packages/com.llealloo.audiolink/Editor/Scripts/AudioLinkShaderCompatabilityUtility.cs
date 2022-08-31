bb2be8e0774dee456bf8bc198958453910f07918
Float3 <86748455+Float3@users.noreply.github.com>
2022-08-31 18:07:33 +0200

    Fixed strings in AudioLink Shader Compatability Utility

--- Packages/com.llealloo.audiolink/Editor/Scripts/AudioLinkShaderCompatabilityUtility.cs
+++ Packages/com.llealloo.audiolink/Editor/Scripts/AudioLinkShaderCompatabilityUtility.cs
@@ -9,17 +9,17 @@ namespace VRCAudioLink.Editor
     {
         private const string OldPath = "Assets/AudioLink/Shaders/AudioLink.cginc";
         private const string NewPath = "Packages/com.llealloo.audiolink/Runtime/Shaders/AudioLink.cginc";
-        private const string MenuItemPath = "AudioLink Menu/Run AudioLink Shader Compatibility Utility";
+        private const string MenuItemPath = "AudioLink/Upgrade AudioLink shaders";
 
         private const string DialogText =
             "Do you want to check all shaders in this project for AudioLink 0.3.x compatibility and update them if necessary? This is useful for upgrading projects using AudioLink 0.2.x or below.?"
             + "\n" + "\n" +
             "If you choose 'Go Ahead', shader files in this project which include 'AudioLink.cginc' will be edited to use the new path introduced by AudioLink 0.3.x. Shaders which already use the new path will be unaffected. You should make a backup before proceeding."
             + "\n" + "\n" +
-            "(You can always run this utility manually via the " + MenuItemPath + "menu command)";
+            "(You can always run this utility manually via the " + MenuItemPath + " menu command)";
 
-        private const string DialogTitle = "I Made a Backup, Go Ahead! ";
-        private const string DialogOkButton = "AudioLink Shader Compatibility Utility";
+        private const string DialogTitle = "AudioLink Shader Compatibility Utility";
+        private const string DialogOkButton = "I Made a Backup, Go Ahead!";
         private const string DialogCancelButton = "No Thanks";
 
 
