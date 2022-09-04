570da3f0710a4ba60f8a4b741ba3b80ea04c6a4a
llealloo <jennygirlinla@gmail.com>
2022-09-04 12:06:32 -0700

    AudioLink_LiveScene: added experimental readback mode toggle pedestals and SET READBACK OFF by default

---
+++ AudioLinkUnityProject/Assets/AudioLinkSandbox/Scripts/AudioLinkExperimentalToggle.cs
@@ -0,0 +1,26 @@
+﻿
+using UdonSharp;
+using UnityEngine;
+using VRC.SDKBase;
+using VRC.Udon;
+
+namespace VRCAudioLink
+{
+    public class AudioLinkExperimentalToggle : UdonSharpBehaviour
+    {
+
+        public UdonBehaviour audioLink;
+
+        void Start()
+        {
+            
+        }
+
+        public override void Interact()
+        {
+            bool toggle = !(bool)audioLink.GetProgramVariable("audioDataToggle");
+            audioLink.SetProgramVariable("audioDataToggle", toggle);
+            InteractionText = "Experimental Readback is " + (string)((toggle == true) ? "ON" : "OFF");
+        }
+    }
+}
