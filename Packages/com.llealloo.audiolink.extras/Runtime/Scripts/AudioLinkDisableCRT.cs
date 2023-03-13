0c25010d3248024aa39a35efb0d9e258b0ab1831
hill <hill@hilll.dev>
2023-03-14 00:13:02 +0100

    disable crt

---
+++ Packages/com.llealloo.audiolink.extras/Runtime/Scripts/AudioLinkDisableCRT.cs
@@ -0,0 +1,32 @@
+﻿#if UDONSHARP
+using UdonSharp;
+
+using UnityEngine;
+
+using VRC.SDKBase;
+using VRC.Udon;
+namespace VRCAudioLink.Extras
+{
+    public class AudioLinkDisableCRT : UdonSharpBehaviour
+    {
+        public AudioLink audioLink;
+        private RenderTexture originalRenderTexture;
+        private int _AudioTexture;
+
+        void Start()
+        {
+            _AudioTexture = VRCShader.PropertyToID("_AudioTexture");
+        }
+
+        public void _DisableCRT() 
+        {
+            VRCShader.SetGlobalTexture(_AudioTexture, null);
+        }
+
+        public void _EnableCRT()
+        {
+            VRCShader.SetGlobalTexture(_AudioTexture, audioLink.audioRenderTexture);
+        }
+    }
+}
+#endif
