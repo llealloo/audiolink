0e89a522c94e9299490a523ea2cf869a8de4c131
hill <hill@hilll.dev>
2023-03-14 16:26:36 +0100

    combine the define manager into the general manager

--- Packages/com.llealloo.audiolink/Editor/Scripts/AudioLinkManager.cs
+++ Packages/com.llealloo.audiolink/Editor/Scripts/AudioLinkManager.cs
@@ -1,6 +1,10 @@
-﻿using System.IO;
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
 using UnityEditor;
 using UnityEditor.SceneManagement;
+
 using UnityEngine;
 
 // This define is inserted into the file in CI.
@@ -97,6 +101,36 @@ namespace VRCAudioLink.Editor
         #endif
     }
 
+    [InitializeOnLoad]
+    public class DefineManager
+    {
+        static DefineManager()
+        {
+            AddDefineIfMissing(EditorUserBuildSettings.selectedBuildTargetGroup, "AUDIOLINK");
+            Shader.EnableKeyword("AUDIOLINK_IMPORTED");
+        }
+
+        private static void AddDefineIfMissing(BuildTargetGroup buildTarget, string newDefine)
+        {
+            bool definesChanged = false;
+            string existingDefines = PlayerSettings.GetScriptingDefineSymbolsForGroup(buildTarget);
+            HashSet<string> defineSet = new HashSet<string>();
+
+            if (existingDefines.Length > 0)
+                defineSet = new HashSet<string>(existingDefines.Split(';'));
+
+            if (defineSet.Add(newDefine))
+                definesChanged = true;
+
+            if (definesChanged)
+            {
+                string finalDefineString = string.Join(";", defineSet.ToArray());
+                PlayerSettings.SetScriptingDefineSymbolsForGroup(buildTarget, finalDefineString);
+                Debug.LogFormat("Set Scripting Define Symbols for selected build target ({0}) to: {1}",
+                    buildTarget.ToString(), finalDefineString);
+            }
+        }
+    }
     public static class SceneManager
     {
         [MenuItem("AudioLink/Add AudioLink Prefab to Scene", false)]
