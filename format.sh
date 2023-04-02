745d165404317f3f1653ecf8e89cfa77ac32ca00
hill <hill@hilll.dev>
2023-04-02 17:16:43 +0200

    add formatting script

---
+++ format.sh
@@ -0,0 +1,4 @@
+dotnet format ./AudioLinkUnityProject/AudioLink.csproj
+dotnet format ./AudioLinkUnityProject/AudioLink.Editor.csproj
+dotnet format ./AudioLinkUnityProject/AudioLink.Extras.csproj
+dotnet format ./AudioLinkUnityProject/AudioLink.Extras.Editor.csproj
