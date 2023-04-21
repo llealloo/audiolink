5b2df0515aeaee264956d4e69557be885aee7655
hill <hill@hilll.dev>
2023-04-21 10:52:06 +0200

    add todo comment

--- Packages/com.llealloo.audiolink/Runtime/Scripts/YtdlpPlayer.cs
+++ Packages/com.llealloo.audiolink/Runtime/Scripts/YtdlpPlayer.cs
@@ -17,6 +17,8 @@ using Debug = UnityEngine.Debug;
 // UnityYoutubePlayer, courtesy iBicha (SPDX-License-Identifier: Unlicense) https://github.com/iBicha/UnityYoutubePlayer
 // USharpVideo, Copyright (c) 2020 Merlin, (SPDX-License-Identifier: MIT) https://github.com/MerlinVR/USharpVideo/
 
+// TODO(float3): add this to the AudioLinkMiniPlayer
+
 namespace AudioLink
 {
     /// <summary> Downloads and plays videos via a VideoPlayer component </summary>
