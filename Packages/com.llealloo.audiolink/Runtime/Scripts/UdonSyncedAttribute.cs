073466becd490f4dc9756326d887c7141b0837de
hill <hill@hilll.dev>
2023-04-27 22:27:35 +0200

    add guard to attribute

--- Packages/com.llealloo.audiolink/Runtime/Scripts/UdonSyncedAttribute.cs
+++ Packages/com.llealloo.audiolink/Runtime/Scripts/UdonSyncedAttribute.cs
@@ -1,3 +1,3 @@
-#if !UDONSHARP
+#if !UDONSHARP && !COMPILER_UDONSHARP
 namespace AudioLink { internal class UdonSyncedAttribute : System.Attribute { } } // stub so we don't have to do the #if UDONSHARP song and dance
 #endif
