245e4977f06f002853fb60dbdcafb61f32250f90
Pema Malling <pemamalling@gmail.com>
2023-05-07 06:38:23 +0200

    Disable this when not UNITY_EDITOR. It prevents you from actually building an assetbundle!

--- Packages/com.llealloo.audiolink/Runtime/Scripts/AudioLink.cs
+++ Packages/com.llealloo.audiolink/Runtime/Scripts/AudioLink.cs
@@ -88,7 +88,9 @@ namespace AudioLink
 
         [Header("Theme Colors")]
         [Tooltip("Enable for custom theme colors for Avatars to use.")]
+        #if UNITY_EDITOR
         [Editor.StringInList("ColorChord Colors", "Custom")]
+        #endif
         public int themeColorMode;
         public Color customThemeColor0 = new Color(1.0f, 1.0f, 0.0f, 1.0f);
         public Color customThemeColor1 = new Color(0.0f, 0.0f, 1.0f, 1.0f);
