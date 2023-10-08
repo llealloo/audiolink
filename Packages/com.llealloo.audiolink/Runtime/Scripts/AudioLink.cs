c90db89679eab32c09d1e28dcf19b89c5694e508
Pema Malling <pemamalling@gmail.com>
2023-10-08 14:58:31 +0200

    Update version numbers

--- Packages/com.llealloo.audiolink/Runtime/Scripts/AudioLink.cs
+++ Packages/com.llealloo.audiolink/Runtime/Scripts/AudioLink.cs
@@ -24,7 +24,7 @@ namespace AudioLink
     public partial class AudioLink : MonoBehaviour
 #endif
     {
-        const float AudioLinkVersionNumber = 1.00f;
+        const float AudioLinkVersionNumber = 1.01f;
 
         [Header("Main Settings")]
         [Tooltip("Should be used with AudioLinkInput unless source is 2D. WARNING: if used with a custom 3D audio source (not through AudioLinkInput), audio reactivity will be attenuated by player position away from the Audio Source")]
