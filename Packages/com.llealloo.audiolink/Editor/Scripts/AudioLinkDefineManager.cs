76925fd0f323885e53ed52a616b72dcfe210599d
hill <hill@hilll.dev>
2024-02-10 22:10:34 +0000

    do this at once instead of calling the same method twice

--- Packages/com.llealloo.audiolink/Editor/Scripts/AudioLinkDefineManager.cs
+++ Packages/com.llealloo.audiolink/Editor/Scripts/AudioLinkDefineManager.cs
@@ -4,38 +4,36 @@ using System.Linq;
 using UnityEditor;
 using UnityEngine;
 
-namespace AudioLink.Editor
-{
+namespace AudioLink.Editor {
     [InitializeOnLoad]
-    public class AudioLinkDefineManager
-    {
-        static AudioLinkDefineManager()
-        {
-            AddDefineIfMissing(EditorUserBuildSettings.selectedBuildTargetGroup, "AUDIOLINK");
-            AddDefineIfMissing(EditorUserBuildSettings.selectedBuildTargetGroup, "AUDIOLINK_V1");
+    public class AudioLinkDefineManager {
+        static AudioLinkDefineManager() {
+            AddDefinesIfMissing(EditorUserBuildSettings.selectedBuildTargetGroup, new string[] { "AUDIOLINK", "AUDIOLINK_V1" });
             Shader.EnableKeyword("AUDIOLINK_IMPORTED");
         }
 
-        private static void AddDefineIfMissing(BuildTargetGroup buildTarget, string newDefine)
-        {
+        private static void AddDefinesIfMissing(BuildTargetGroup buildTarget, params string[] newDefines) {
             bool definesChanged = false;
             string existingDefines = PlayerSettings.GetScriptingDefineSymbolsForGroup(buildTarget);
             HashSet<string> defineSet = new HashSet<string>();
 
-            if (existingDefines.Length > 0)
+            if (existingDefines.Length > 0) {
                 defineSet = new HashSet<string>(existingDefines.Split(';'));
+            }
 
-            if (defineSet.Add(newDefine))
-                definesChanged = true;
+            foreach (string newDefine in newDefines) {
+                if (defineSet.Add(newDefine)) {
+                    definesChanged = true;
+                }
+            }
 
-            if (definesChanged)
-            {
+            if (definesChanged) {
                 string finalDefineString = string.Join(";", defineSet.ToArray());
                 PlayerSettings.SetScriptingDefineSymbolsForGroup(buildTarget, finalDefineString);
-                Debug.LogFormat("Set Scripting Define Symbols for selected build target ({0}) to: {1}",
-                    buildTarget.ToString(), finalDefineString);
+                Debug.LogFormat("Set Scripting Define Symbols for selected build target ({0}) to: {1}", buildTarget.ToString(), finalDefineString);
             }
         }
     }
+
 }
 #endif
