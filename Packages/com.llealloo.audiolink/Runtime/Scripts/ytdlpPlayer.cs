0ecfbccbcccc833981271cd99a9f857573a53fb1
TechAnon <git@techanon.dev>
2024-04-16 00:53:53 -0500

    Default the resolution to the highest. Not really any reason to limit to 720p in editor. Fix the video player restarting the media 3 seconds in issue by offloading the video player Play action to the prepareCompleted event.

--- Packages/com.llealloo.audiolink/Runtime/Scripts/ytdlpPlayer.cs
+++ Packages/com.llealloo.audiolink/Runtime/Scripts/ytdlpPlayer.cs
@@ -23,7 +23,7 @@ namespace AudioLink
 
         public VideoPlayer videoPlayer = null;
 
-        [SerializeField] public Resolution resolution = Resolution._720p;
+        [SerializeField] public Resolution resolution = Resolution._2160p;
 
         public enum Resolution
         {
@@ -59,12 +59,16 @@ namespace AudioLink
             if (videoPlayer == null)
                 return;
 
+            videoPlayer.prepareCompleted -= MediaReady;
+            videoPlayer.prepareCompleted += MediaReady;
             videoPlayer.url = resolved;
-            SetPlaybackTime(0.0f);
-            if (videoPlayer.length > 0)
-            {
-                videoPlayer.Play();
-            }
+            videoPlayer.Prepare();
+        }
+
+        private void MediaReady(VideoPlayer player)
+        {
+            SetPlaybackTime(player, 0.0f);
+            if (player.length > 0) player.Play();
         }
 
         public float GetPlaybackTime()
@@ -75,10 +79,10 @@ namespace AudioLink
                 return 0;
         }
 
-        public void SetPlaybackTime(float time)
+        public void SetPlaybackTime(VideoPlayer player, float time)
         {
-            if (videoPlayer != null && videoPlayer.length > 0 && videoPlayer.canSetTime)
-                videoPlayer.time = videoPlayer.length * Mathf.Clamp(time, 0.0f, 1.0f);
+            if (player != null && player.length > 0 && player.canSetTime)
+                player.time = player.length * Mathf.Clamp(time, 0.0f, 1.0f);
         }
 
         public string FormattedTimestamp(double seconds, double maxSeconds = 0)
@@ -379,7 +383,7 @@ namespace AudioLink
                         EditorGUI.BeginChangeCheck();
                         playbackTime = GUILayout.HorizontalSlider(playbackTime, 0, 1);
                         if (EditorGUI.EndChangeCheck())
-                            _ytdlpPlayer.SetPlaybackTime(playbackTime);
+                            _ytdlpPlayer.SetPlaybackTime(_ytdlpPlayer.videoPlayer, playbackTime);
 
                         // Timestamp input
                         EditorGUI.BeginChangeCheck();
@@ -396,7 +400,7 @@ namespace AudioLink
                             if (TimeSpan.TryParse($"00:{seekTimestamp}", out inputTimestamp))
                             {
                                 playbackTime = (float)(inputTimestamp.TotalSeconds / videoLength);
-                                _ytdlpPlayer.SetPlaybackTime(playbackTime);
+                                _ytdlpPlayer.SetPlaybackTime(_ytdlpPlayer.videoPlayer, playbackTime);
                             }
                         }
                     }
