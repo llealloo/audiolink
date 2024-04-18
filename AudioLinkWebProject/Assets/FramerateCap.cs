2a9628349a3a1600f58b48d61e04c15d3ba52f5a
fundale <69996669+fundale@users.noreply.github.com>
2024-04-18 15:39:42 -0500

    Minor Tweaks

--- AudioLinkWebProject/Assets/FramerateCap.cs
+++ AudioLinkWebProject/Assets/FramerateCap.cs
@@ -1,16 +1,24 @@
-﻿using System.Collections;
+﻿#if !UDONSHARP
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+#if AUDIOLINK_V1
 using AudioLink;
+#endif
 
 public class FramerateCap : MonoBehaviour
 {
+    #if AUDIOLINK_V1
     public AudioLink.AudioLink audioLink;
-    public int limit = 90;
-    // Start is called before the first frame update
+    #endif
+    public int limit = 60;
+    
     void Start()
     {
         Application.targetFrameRate = limit;
+        #if AUDIOLINK_V1
         if (audioLink != null) audioLink.audioRenderTexture.updatePeriod = 1 / (float)limit;
+        #endif
     }
 }
+#endif
