616011d8cd909c91a67bb0860775e65a0bcc1c81
fundale <69996669+fundale@users.noreply.github.com>
2024-04-18 14:30:44 -0500

    Add AudioLink Web Project (without AudioLink hook)

---
+++ AudioLinkWebProject/Assets/FramerateCap.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using AudioLink;
+
+public class FramerateCap : MonoBehaviour
+{
+    public AudioLink.AudioLink audioLink;
+    public int limit = 90;
+    // Start is called before the first frame update
+    void Start()
+    {
+        Application.targetFrameRate = limit;
+        if (audioLink != null) audioLink.audioRenderTexture.updatePeriod = 1 / (float)limit;
+    }
+}
