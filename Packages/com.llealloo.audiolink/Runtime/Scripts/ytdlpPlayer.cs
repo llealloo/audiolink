ecc3d5f4c4770d9ecfda79f13ee0e138611b64c4
BluWizard <17975114+BluWizard10@users.noreply.github.com>
2024-05-07 20:55:35 -0700

    Change default Ytdlp URL to something more tolerate

--- Packages/com.llealloo.audiolink/Runtime/Scripts/ytdlpPlayer.cs
+++ Packages/com.llealloo.audiolink/Runtime/Scripts/ytdlpPlayer.cs
@@ -24,7 +24,7 @@ namespace AudioLink
             ByPixels
         }
 
-        public string ytdlpURL = "https://www.youtube.com/watch?v=SFTcZ1GXOCQ";
+        public string ytdlpURL = "https://www.youtube.com/watch?v=dZPcLi2-5dE";
         ytdlpRequest _currentRequest = null;
 
         public bool showVideoPreviewInComponent = false;
