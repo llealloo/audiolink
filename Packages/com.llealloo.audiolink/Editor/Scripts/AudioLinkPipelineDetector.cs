d7cacb9cbaf52b3e3720b97671d16cb83816d4c4
TechAnon <git@techanon.dev>
2025-06-23 03:16:01 -0500

    Add namespace to pipeline detector

--- Packages/com.llealloo.audiolink/Editor/Scripts/AudioLinkPipelineDetector.cs
+++ Packages/com.llealloo.audiolink/Editor/Scripts/AudioLinkPipelineDetector.cs
@@ -1,21 +1,24 @@
 using UnityEngine;
 using UnityEditor;
 
-public static class AudioLinkPipelineDetector
+namespace AudioLink.Editor
 {
-    [InitializeOnLoadMethod]
-    private static void DetectPackages()
+    public static class AudioLinkPipelineDetector
     {
+        [InitializeOnLoadMethod]
+        private static void DetectPackages()
+        {
 #if URP_AVAILABLE
         Shader.EnableKeyword("UNIVERSAL_RENDER_PIPELINE");
 #else
-        Shader.DisableKeyword("UNIVERSAL_RENDER_PIPELINE");
+            Shader.DisableKeyword("UNIVERSAL_RENDER_PIPELINE");
 #endif
 
 #if HDRP_AVAILABLE
         Shader.EnableKeyword("HIGH_DEFINITION_RENDER_PIPELINE");
 #else
-        Shader.DisableKeyword("HIGH_DEFINITION_RENDER_PIPELINE");
+            Shader.DisableKeyword("HIGH_DEFINITION_RENDER_PIPELINE");
 #endif
+        }
     }
 }
