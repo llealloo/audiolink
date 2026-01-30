06e048a403e6b3917695d74ad341aae145d34eb4
fundale <69996669+fundale@users.noreply.github.com>
2026-01-30 12:12:37 -0600

    Fix compile error on first import

---
+++ Packages/com.llealloo.audiolink/Runtime/Scripts/AudioReactivePostProcessing.cs
@@ -0,0 +1,43 @@
+﻿#if UNITY_POST_PROCESSING_STACK_V2
+using UnityEngine;
+using UnityEngine.Rendering.PostProcessing;
+
+namespace AudioLink
+{
+#if UDONSHARP
+    using UdonSharp;
+
+    [RequireComponent(typeof(PostProcessVolume))]
+    public class AudioReactivePostProcessing : UdonSharpBehaviour
+#else
+    [RequireComponent(typeof(PostProcessVolume))]
+    public class AudioReactivePostProcessing : MonoBehaviour
+#endif
+    {
+        public AudioLink audioLink;
+        public int band;
+        [Range(0, 127)]
+        public int delay;
+        public float weight = 1;
+        public float minWeight = 0;
+
+        private PostProcessVolume _postProcessVolume;
+
+        void Start()
+        {
+            _postProcessVolume = transform.GetComponent<PostProcessVolume>();
+
+        }
+
+        void Update()
+        {
+            if (audioLink.AudioDataIsAvailable())
+            {
+                float amplitude = audioLink.GetDataAtPixel(delay, band).x;
+                _postProcessVolume.weight = Mathf.Lerp(minWeight, weight, amplitude);
+            }
+        }
+
+    }
+}
+#endif
